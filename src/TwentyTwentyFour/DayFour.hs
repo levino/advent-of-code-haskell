983df3f3b727b47cba5d59f568f67576c94b4c39
Levin Keller <post@levinkeller.de>
2024-12-06 12:04:15 +0000

    day 4 part 2

--- src/TwentyTwentyFour/DayFour.hs
+++ src/TwentyTwentyFour/DayFour.hs
@@ -1,13 +1,33 @@
-module TwentyTwentyFour.DayFour (countXmas) where 
+module TwentyTwentyFour.DayFour (countXmas) where
 
 import Data.Function ((&))
 
-
 countXmas :: String -> Int
-countXmas input = input & lines & (\rows -> 
-    lines & zip [1..] & 
-    foldl (\numberOfXmasInLastRow (rowNumber, line) -> 
-        line & zip [1..] & foldl (\numberOfXmasInCurrentRow (columnNumber, char) -> 0) 0 )
-        0  
-        )
-     
+countXmas input =
+  input
+    & lines
+    & ( \rows ->
+          rows
+            & zip [0 ..]
+            & foldl
+              ( \numberOfXmasInLastRow (rowNumber, line) ->
+                  numberOfXmasInLastRow
+                    + ( zip [0 ..] line
+                          & foldl
+                            ( \numberOfXmasInCurrentRow (columnNumber, char) ->
+                                ( if rowNumber == 0 || columnNumber == 0 || rowNumber == length rows - 1 || columnNumber == length line - 1 || char /= 'A'
+                                    then numberOfXmasInCurrentRow
+                                    else
+                                      let mas1 = [rows !! (rowNumber - 1) !! (columnNumber - 1), char, rows !! (rowNumber + 1) !! (columnNumber + 1)]
+                                          mas2 = [rows !! (rowNumber - 1) !! (columnNumber + 1), char, rows !! (rowNumber + 1) !! (columnNumber - 1)]
+                                          xMas = (mas1 == "MAS" || mas1 == "SAM") && (mas2 == "MAS" || mas2 == "SAM")
+                                       in if xMas
+                                            then numberOfXmasInCurrentRow + 1
+                                            else numberOfXmasInCurrentRow
+                                )
+                            )
+                            0
+                      )
+              )
+              0
+      )
